91c41436d2f12caf5c7eba7457642cf0d8ea3434
Alexander Milovsky <alex@milovsky.ru>
2020-07-23 12:26:35 +0300

    GPL headers added

--- faceutils/__init__.py
+++ faceutils/__init__.py
@@ -1,3 +1,23 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+# KTFaceRig add-on for Blender is an unofficial Face Rig add-on
+# working with KeenTools FaceBuilder model.
+# Copyright (C) 2020 Alexander Milovsky
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ##### END GPL LICENSE BLOCK #####
+
+
 import bpy
 import mathutils
 from mathutils import Vector
